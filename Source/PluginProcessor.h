fb5ac3271a52dc3a82079f63d9f7f8631e7e41b9
jgagl <jonathan.gaglione@hotmail.com>
2025-01-08 19:38:51 +0100

    Added DSP

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -58,6 +58,14 @@ class JG_JEQAudioProcessor  : public juce::AudioProcessor
     juce::AudioProcessorValueTreeState apvts {*this, nullptr, "Parameters", createParameterLayout()};
 
 private:
+    using Filter = juce::dsp::IIR::Filter<float>;
+    
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+    
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+    
+    MonoChain leftChain, rightChain;
+    
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (JG_JEQAudioProcessor)
 };
